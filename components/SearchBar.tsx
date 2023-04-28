57558145572a5884f324e048b93beaf2a43edeb3
Jatin <dmohit0026@gmail.com>
2023-04-28 10:32:39 +0530

    home screen

---
+++ components/SearchBar.tsx
@@ -0,0 +1,22 @@
+interface interfaceSearchBar {
+  value: string;
+  handleChange: any;
+}
+const SearchBar = ({ handleChange, value }: interfaceSearchBar) => {
+  return (
+    <div className=' flex flex-row items-center w-3/4 rounded-lg justify-between  border-2 border-gray-500 '>
+      <input
+        value={value}
+        className=' h-12 px-4 pr-12 grow bg-black ml-1 focus:outline-none text-white focus:outline-none '
+        type='text'
+        placeholder='Type in ETH Address..'
+        onChange={handleChange}
+      />
+      <button className='border-l-2 border-gray-500 px-4 h-12  font-inter font-medium text-white text-base leading-6 flex items-center justify-center '>
+        Generate
+      </button>
+    </div>
+  );
+};
+
+export default SearchBar;
--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,8 +1,10 @@
 import Login from "@/components/Login";
 import Nav from "@/components/Nav";
+import SearchBar from "@/components/SearchBar";
 import React, { useState } from "react";
 
 function Dashboard() {
+  const [ETHAddress, setETHAddress] = useState("");
   const [show, setShow] = useState(false);
   const handleClose = () => {
     setShow(false);
@@ -11,10 +13,32 @@ function Dashboard() {
     setShow(true);
   };
 
+  const handleChange = (event: any) => {
+    setETHAddress(event.target.value);
+  };
   return (
     <div className='flex min-h-screen flex-col items-center bg-#0A0909'>
       <Nav handleOpen={handleOpen} />
       <Login handleClose={handleClose} show={show} />
+      <div className='flex flex-col items-center my-20 '>
+        <img
+          src='/Watchdogs.png'
+          alt='Watchdogs'
+          className='w-auto lg:h-24 md:h-20 sm:h-16 mx-auto mb-4'
+        />
+        <div className='font-inter font-normal font-light text-2xl leading-10 flex items-center text-center text-white'>
+          We know what you do in the dark..
+        </div>
+      </div>
+      <div className='relative flex flex-col items-center w-full h-72'>
+        <div
+          className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-72 justify-self-center h-72 inset-0 bg-cover bg-center'
+          style={{ backgroundImage: "url('/homeBG.png')" }}
+        />
+        <div className='absolute w-full top-28 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 flex flex-col items-center'>
+          <SearchBar value={ETHAddress} handleChange={handleChange} />
+        </div>
+      </div>
     </div>
   );
 }
